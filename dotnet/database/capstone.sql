5511a63955b7589df79be09dfa6528c24ee7825e
Alexander Stuart <ahdisease@gmail.com>
2023-10-03 13:17:53 -0400

    created bird and bird_sighting tables to schema

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -20,9 +20,46 @@ CREATE TABLE users (
 	username varchar(50) NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	salt varchar(200) NOT NULL,
-	user_role varchar(50) NOT NULL
+	user_role varchar(50) NOT NULL,
+	location varchar(50),
+	skill_level varchar(20) NOT NULL DEFAULT 'beginner',	--beginner, intermediate, pro
+	favorite_bird int,
+	most_common_bird int
+
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE bird (
+	id int IDENTITY(1,1) NOT NULL UNIQUE,
+	name varchar(50) NOT NULL,
+	description varchar(1000),
+	picture varchar(max)
+
+	CONSTRAINT PK_bird PRIMARY KEY (id)
 )
+
+CREATE TABLE bird_sighting (
+	id int IDENTITY(1,1) NOT NULL,
+	user_id int NOT NULL,
+	bird_id int NOT NULL,
+	date_sighted date NOT NULL DEFAULT GETDATE()
+
+	CONSTRAINT PK_bird_sighting PRIMARY KEY (id)
+)
+
+--add foreign key contraints
+ALTER TABLE users
+	ADD CONSTRAINT FK_favorite_bird FOREIGN KEY (favorite_bird) REFERENCES bird (id);
+
+ALTER TABLE users
+	ADD CONSTRAINT FK_most_common_bird FOREIGN KEY (most_common_bird) REFERENCES bird (id);
+
+ALTER TABLE bird_sighting
+	ADD CONSTRAINT FK_bird_sighting_users FOREIGN KEY (user_id) REFERENCES users (user_id);
+
+ALTER TABLE bird_sighting
+	ADD CONSTRAINT FK_bird_sighting_users FOREIGN KEY (bird_id) REFERENCES bird (id);
+
 
 --populate default data
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
