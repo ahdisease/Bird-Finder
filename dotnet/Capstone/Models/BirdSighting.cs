7d24c2949e08f040be82b59a8092727bc827e3b9
kimbambala <nickprincy@gmail.com>
2023-10-05 15:53:07 -0700

    Base DAO are set for Bird sightings

--- dotnet/Capstone/Models/BirdSighting.cs
+++ dotnet/Capstone/Models/BirdSighting.cs
@@ -1,6 +1,14 @@
-﻿namespace Capstone.Models
+﻿using System;
+
+namespace Capstone.Models
 {
     public class BirdSighting
     {
+        public int id {  get; set; }
+        public int userId { get; set; }
+        public int birdId {  get; set; }
+        public DateTime dateSighted { get; set; }
     }
+
+
 }
