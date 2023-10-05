986a945ee48c91c13334c22b4640f831ddb0642f
kimbambala <nickprincy@gmail.com>
2023-10-05 16:44:52 -0700

    working on the DAO for the list of bird sighted per bird_id

--- dotnet/Capstone/DAO/IBirdSightingDao.cs
+++ dotnet/Capstone/DAO/IBirdSightingDao.cs
@@ -1,6 +1,14 @@
-﻿namespace Capstone.DAO
+﻿using Capstone.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Capstone.DAO
 {
     public interface IBirdSightingDao
     {
+        List<BirdSighting> getSightings(int birdId);
+        void deleteSighting(int  id);
+        void editSighting(BirdSighting sighting, int id);
+        BirdSighting addSighting(BirdSighting birdSighting, int userId, int birdId, DateTime dateSighted);
     }
 }
