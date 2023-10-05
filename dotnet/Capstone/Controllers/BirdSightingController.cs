1aafbdebbfa7f43ae7295f3200b03e2e2d20bf63
kimbambala <nickprincy@gmail.com>
2023-10-05 16:45:58 -0700

    Finished working on the controller part for the method to retrieve a list of bird sighted per bird_id

---
+++ dotnet/Capstone/Controllers/BirdSightingController.cs
@@ -0,0 +1,41 @@
+﻿using Capstone.DAO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using Capstone.Models;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BirdSightingController : Controller
+    {
+        private readonly IBirdSightingDao birdSightingDao;
+        public BirdSightingController(IBirdSightingDao birdSightingDao)
+        {
+            this.birdSightingDao = birdSightingDao;
+
+        }
+
+        [HttpGet("/birds/{birdId}/notes")]
+        public List<BirdSighting> ListAllBirds(int birdId)
+        {
+            List<BirdSighting> sightingList = birdSightingDao.getSightings(birdId);
+
+            if (sightingList == null)
+            {
+                Console.WriteLine("No bird");
+            }
+
+            return sightingList;
+
+        }
+
+
+
+
+
+
+    }
+}
