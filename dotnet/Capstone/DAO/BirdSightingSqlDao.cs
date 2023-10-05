7a53cc4a3f47df148d402ce1b2f4613b5111d9e7
kimbambala <nickprincy@gmail.com>
2023-10-05 16:43:29 -0700

    Finished working on method to retrive a list of birds sightings based on a bird_id

--- dotnet/Capstone/DAO/BirdSightingSqlDao.cs
+++ dotnet/Capstone/DAO/BirdSightingSqlDao.cs
@@ -1,6 +1,78 @@
-﻿namespace Capstone.DAO
+﻿using Capstone.Exceptions;
+using Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Capstone.DAO
 {
-    public class BirdSightingSqlDao
+    public class BirdSightingSqlDao : IBirdSightingDao
+
     {
+        private readonly string connectionString;
+
+        public BirdSightingSqlDao(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+
+
+        public BirdSighting addSighting(BirdSighting birdSighting, int userId, int birdId, DateTime dateSighted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteSighting(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void editSighting(BirdSighting sighting, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BirdSighting> getSightings(int birdId)
+        {
+            List<BirdSighting> sightingList = new List<BirdSighting>();
+
+            string sql = "SELECT id, user_id, bird_id, date_sighted FROM bird_sighting WHERE bird_id = @bird_id";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@bird_id", birdId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        BirdSighting birdSighting = MapRowToBirdSighting(reader);
+                        sightingList.Add(birdSighting);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new DaoException("SQL exception occurred", ex);
+            }
+
+            return sightingList;
+        }
+
+        private BirdSighting MapRowToBirdSighting(SqlDataReader reader)
+        {
+            BirdSighting birdSighting = new BirdSighting();
+            birdSighting.id = Convert.ToInt32(reader["id"]);
+            birdSighting.userId = Convert.ToInt32(reader["user_id"]);
+            birdSighting.birdId = Convert.ToInt32(reader["bird_id"]);
+            birdSighting.dateSighted = Convert.ToDateTime(reader["date_sighted"]);
+
+            return birdSighting;
+        }
     }
 }
