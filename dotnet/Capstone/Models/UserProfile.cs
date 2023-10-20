1a3dfcd8ac78eab527a0c5c2108833fecbb5c37e
Alexander Stuart <ahdisease@gmail.com>
2023-10-20 13:28:53 -0400

    refined model validation

--- dotnet/Capstone/Models/UserProfile.cs
+++ dotnet/Capstone/Models/UserProfile.cs
@@ -5,17 +5,16 @@ namespace Capstone.Models
     public class UserProfile
     {
         [Required]
-        [MaxLength(9)]
-        [MinLength(5)]
+        [RegularExpression("^\\d{5}(?:[-\\s]\\d{4})?$",ErrorMessage ="Parameter zipCode must be a valid zip or zip+4 format.")]
         public string ZipCode { get; set; }
         [Required]
-        [RegularExpression("(beginner|intermediate|advanced)")]
+        [RegularExpression("(beginner|intermediate|advanced)",ErrorMessage = "Parameter skillLevel only accepts 'beginner', 'intermediate' and 'advanced' as parameters.")]
         public string SkillLevel { get; set; }
         [Required]
-        [MaxLength(20)]
+        [MaxLength(20,ErrorMessage = "Parameter favoriteBird must be 20 characters or less.")]
         public string FavoriteBird { get; set; }
         [Required]
-        [MaxLength(20)]
+        [MaxLength(20, ErrorMessage = "Parameter mostCommonBird must be 20 characters or less.")]
         public string MostCommonBird { get; set; }
         public bool ProfileActive { get; set; }
 
